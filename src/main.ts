7d8c62019dede92e19e8a05fcc0b25ff76d75cbe
zherunliu <1437257281@qq.com>
2025-07-17 15:21:53 +0800

    some settings

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import './style.less'
 import App from './App.vue'
 import router from './router'
-import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -11,12 +10,11 @@ import '@/router/guard'
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
+    app.component(key, component)
 }
 const pinia = createPinia()
 
 app.use(router)
 app.use(pinia)
-app.use(ElementPlus)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    // 按需导入 element-plus
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       // '@': path.resolve(__dirname, './src'),
