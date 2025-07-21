6efa278c856570fd281c8ec5f07bff7e3a5b9832
hangtiancheng <hangtiancheng@bytedance.com>
2025-07-21 23:18:22 +0800

    chore: 💗 Add some helpful comments

--- src/layouts/TabsLayout.vue
+++ src/layouts/TabsLayout.vue
@@ -27,42 +27,57 @@ import type { MenuItem } from '@/types/user'
 import { storeToRefs } from 'pinia'
 import { useRouter, useRoute } from 'vue-router'
 import { useUserStore } from '@/store/auth'
+import { type TabsPaneContext, type TabPaneName } from 'element-plus'
 
 const router = useRouter()
 const route = useRoute()
 const tabsStore = useTabsStore()
 const userStore = useUserStore()
+
+// ref/reactive 定义的响应式变量需要使用 storeToRefs, toRef 或 toRefs 解构
 const { menu } = storeToRefs(userStore)
 // currentTab 设置当前高亮
 const { tabs, currentTab } = storeToRefs(tabsStore)
+
+// 函数 (方法) 可以直接解构
 const { setCurrentTab, addTab, removeTab } = tabsStore
 
-const handleClick = ({ index: idx }: { index: string | undefined }) => {
+// TabsPaneContext 标准的类型, 从 element-plus 中导入
+const handleClick = ({ index: idx }: TabsPaneContext) => {
   const index = idx ? Number.parseInt(idx, 10) : NaN
   router.push(tabs.value[index].url)
   setCurrentTab(tabs.value[index].name, tabs.value[index].url)
 }
-const remove = (TabPaneName: string | number) => {
-  removeTab(TabPaneName.toString())
+
+// TabsPaneContext 标准的类型, 从 element-plus 中导入
+// type TabPaneName = string | number
+const remove = (tabPaneName: TabPaneName) => {
+  removeTab(tabPaneName.toString())
   router.push(currentTab.value.url)
 }
+
 function findObjectByUrl(arr: MenuItem[], url: string): MenuItem | undefined {
   for (const item of arr) {
     if (item.url === url) {
       return item
     }
     if (item.children) {
-      const found = findObjectByUrl(item.children, url)
-      if (found) {
-        return found
-      }
+      const found: MenuItem | undefined = findObjectByUrl(item.children, url)
+      // if (found) {
+      //   return found
+      // }
+      return found // MenuItem | undefined, 这里没有必要做空判断
     }
   }
   return undefined
 }
-console.log(route.path)
 const item = findObjectByUrl(menu.value, route.path)
-console.log(item)
+
+if (import.meta.env.DEV) {
+  console.log(route.path)
+  console.log(item)
+}
+
 if (item) {
   const { name, url, icon } = item
   addTab(name, url, icon)
