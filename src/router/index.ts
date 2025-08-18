d622523c2ad60fa262add5075f3430c34af56797
zherunliu <1437257281@qq.com>
2025-08-18 23:46:30 +0800

    feat: base

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from './basicRouteMap'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/ldv'),
   routes,
 })
 
