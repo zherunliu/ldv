26156e4b35ff95e12944f6c76ccaf35abc5f7e4e
hangtiancheng <hangtiancheng@bytedance.com>
2025-08-19 14:10:47 +0800

    feat: Update guard action, use different router prefix under dev/prod mode

--- vite.config.ts
+++ vite.config.ts
@@ -10,25 +10,26 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/ldv/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-    // 按需导入 element-plus
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    // pnpm build
-    visualizer({ open: true }),
-  ],
-  resolve: {
-    alias: {
-      // '@': path.resolve(__dirname, './src'),
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default ({ mode }: { mode: string }) =>
+  defineConfig({
+    base: mode == 'development' ? '/' : '/ldv/',
+    plugins: [
+      vue(),
+      vueDevTools(),
+      // 按需导入 element-plus
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      // pnpm build
+      visualizer({ open: true }),
+    ],
+    resolve: {
+      alias: {
+        // '@': path.resolve(__dirname, './src'),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-})
+  })
